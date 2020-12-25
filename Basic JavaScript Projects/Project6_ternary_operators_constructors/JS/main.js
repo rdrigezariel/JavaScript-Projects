f27f8c8bfa4f050fedf2795e32d1dfc48fb2f365
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2020-12-24 19:53:11 -0500

    Create Project 6 Ternary Operators

---
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,61 @@
+function rideFunction() {
+    var height, canRide;
+    height = document.getElementById("height").value;
+    canRide = (height < 52) ? "You are too short" : "You are tall enough";
+    document.getElementById("ride").innerHTML = canRide + " to ride.";
+}
+
+function voteFunction() {
+    var age, canVote;
+    age = document.getElementById("age").value;
+    canVote = (age < 18) ? "You are NOT old enough" : "You ARE old enough";
+    document.getElementById("vote").innerHTML = canVote + " to vote!";
+}
+
+function Vehicle(make, model, year, color) {
+    this.vehicleMake = make;
+    this.vehicleModel = model;
+    this.vehicleYear = year;
+    this.vehicleColor = color;
+}
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+var Ariel = new Vehicle("Honda", "Accord", 2014, "Grey");
+
+function myFunction() {
+    document.getElementById("keywordsAndConstructors").innerHTML =
+    "Erik drives a " + Erik.vehicleColor + "-colored " + Erik.vehicleModel +
+    " manufactured in " + Erik.vehicleYear;
+}
+
+function mySecondFunction() {
+    document.getElementById("newAndThis").innerHTML =
+    "Ariel drives a " + Ariel.vehicleColor + "-colored " + Ariel.vehicleModel +
+    " manufactured in " + Ariel.vehicleYear;
+}
+
+// function reservedWordFunc() {
+//     var true = true;
+//     document.getElementById("reservedWord").innerHTML = true;
+// }
+
+function Person(firstName, lastName, age, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.dob = dob
+}
+
+function nestedFunction() {
+    document.getElementById("nestFunction").innerHTML = count();
+    function count() {
+        var startingPoint = 0;
+        function plusOne() {
+            startingPoint += 1;
+        }
+        plusOne();
+        return startingPoint;
+    }
+}
